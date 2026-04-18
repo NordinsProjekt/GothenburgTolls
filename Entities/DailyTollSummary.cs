e8c0916755eef9ce35c1b106c8bbafe1e7e7b443
Markus Nordin <belfegorc4@gmail.com>
2026-04-18 19:37:30 +0200

    Adding entities

---
+++ Entities/DailyTollSummary.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities;
+
+public class DailyTollSummary(DateOnly forDay, decimal amount)
+{
+    public Guid Id { get; set; }
+    public DateTime Created { get; }
+    public DateOnly ForDay { get; } = forDay;
+    public decimal Amount { get; set; } = amount;
+
+    protected List<TollEvent> TollEvents { get; set; } = new();
+}
