42888993ce0c31fdd2808bf14c12661171f2d637
Markus Nordin <belfegorc4@gmail.com>
2026-04-18 22:56:17 +0200

    Adding configurations, Interfaces and Repos

---
+++ EF/Configurations/VehicleConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Entities;
+using Entities.Bases;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EF.Configurations;
+
+internal class VehicleConfiguration : IEntityTypeConfiguration<Vehicle>
+{
+    public void Configure(EntityTypeBuilder<Vehicle> e)
+    {
+        e.ToTable("Vehicles");
+        e.HasKey(v => v.Id);
+
+        e.Property(v => v.RegistrationNumber)
+            .HasMaxLength(12)
+            .IsRequired();
+
+        e.HasIndex(v => v.RegistrationNumber).IsUnique();
+
+        e.HasDiscriminator<string>("VehicleType")
+            .HasValue<Car>(nameof(Car))
+            .HasValue<Motorbike>(nameof(Motorbike));
+    }
+}
