b4e474123a4ff2bb1f622ead9b10c026d7a9d25b
Markus Nordin <belfegorc4@gmail.com>
2026-04-18 20:31:25 +0200

    Fixes access property

--- Entities/TollEvent.cs
+++ Entities/TollEvent.cs
@@ -4,17 +4,15 @@ namespace Entities;
 
 public class TollEvent
 {
-    private readonly Guid vehicle;
-
     public Guid Id { get; set; }
 
-    public DateTime EventDateTime { get; set; }
+    public DateTime EventDateTime { get; }
 
-    public Guid? VehicleId { get; set; }
-    public Vehicle? Vehicle { get; set; }
+    public Guid? VehicleId { get; }
+    public Vehicle? Vehicle { get; }
 
-    public Guid? DailyTollSummaryId { get; set; }
-    public DailyTollSummary? DailyTollSummary { get; set; }
+    public Guid? DailyTollSummaryId { get; }
+    public DailyTollSummary? DailyTollSummary { get; }
 
     //For EF
     private TollEvent() { }
