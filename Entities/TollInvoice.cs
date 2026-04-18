74a2004d6dfcd91c3f37e788363751df94c1e571
Markus Nordin <belfegorc4@gmail.com>
2026-04-18 19:52:23 +0200

    Adding the last entities

---
+++ Entities/TollInvoice.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities;
+
+public class TollInvoice
+{
+    public Guid Id { get; }
+
+    public DateOnly FromDay { get; }
+    public DateOnly ToDay { get; }
+    protected decimal Sum => TollSummary.Sum(ts => ts.Amount);
+
+    protected List<DailyTollSummary> TollSummary { get; set; } = new();
+
+}
