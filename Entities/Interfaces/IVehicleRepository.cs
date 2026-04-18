2a0020e43e2986269309199849ccd86cf36eb9b7
Markus Nordin <belfegorc4@gmail.com>
2026-04-18 17:10:48 +0200

    Adding Interfaces and types

---
+++ Entities/Interfaces/IVehicleRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Bases;
+using Entities.Types;
+
+namespace Entities.Interfaces;
+
+public interface IVehicleRepository
+{
+    Task<Vehicle> GetAllVehicleAsync();
+    Task<Vehicle> GetVehicleByIdAsync(Guid id);
+    Task<Vehicle> GetVehicleByType(VehicleType type);
+
+    Task<Guid> CreateVehicle(Vehicle vehicle);
+}
